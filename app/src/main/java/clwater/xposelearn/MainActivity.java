0804b92daa04d85a6fa3a5f5215a05de27f31ca5
gengzhibo <gengzhibo@gengzhibos-MacBook-Pro.local>
2017-07-13 14:54:21 +0800

    20170713

--- app/src/main/java/clwater/xposelearn/MainActivity.java
+++ app/src/main/java/clwater/xposelearn/MainActivity.java
@@ -2,6 +2,7 @@ package clwater.xposelearn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textview);
         textView.setText("main");
+
+        Log.i("gzb", "before inflate");
+        getLayoutInflater().inflate(R.layout.activity_main2, null);
+        Log.i("gzb", "after inflate");
     }
 }
